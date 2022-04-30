f36961bb23adcacaf99ae8c176a1da1fd833cd24
calm1205 <naginani072@gmail.com>
2022-04-30 19:12:01 +0900

    React 18 render修正

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(
+const rootDOM = document.getElementById("root") as HTMLElement;
+const root = ReactDOM.createRoot(rootDOM);
+
+root.render(
   <React.StrictMode>
     <h1>hello react</h1>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
